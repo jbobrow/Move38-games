c0a88eb0a20359e1f6491c9361ae55312f50a5e8
Jonathan Bobrow <jbobrow@gmail.com>
2016-11-02 01:58:51 -0400

    pulse period changes with health. Can't find bug where period becomes erradic after n seconds... seems to take 50 seconds to die instead of expected 20 seconds

--- TerritoryControl/TerritoryControl.ino
+++ TerritoryControl/TerritoryControl.ino
@@ -2,6 +2,9 @@
  *  
  *  Setup: 2 player game. Tiles die slowly over time. Quicker when surrounded by enemies, and slower when surrounded by family. Moving a tile boosts its life.
  *    
+ *  When a tile is moved, it regains 20% health  
+ *  When a tile hits an opponents tile, the opponents tile takes a 20% damage
+ *    
  *  Technical Details:
  *    A long press on the tile changes the color of the tile for prototyping (switch state 1 or 2)
  *    
@@ -37,13 +40,11 @@ enum gameStates {
   start
 };
 
-uint8_t colors[5][3] = {{255,0,0},      // red   (player 1)
-                    {0,0,255},      // blue  (player 2)
-                    {255,255,255},  // white (dead state)
-                    {255,255,255},  // white (wait state)
-                    {0,255,0}};     // green (start state)
+uint8_t colors[3][3] = {{255,0,0},     // red   (player 1)
+                       {0,0,255},      // blue  (player 2)
+                       {255,255,255}}; // white (dead state)
 
-int brightness[60] = {
+uint8_t brightness[60] = {
   64,71,77,84,90,96,102,107,
   112,116,119,122,125,127,128,128,
   128,127,125,122,119,116,112,107,
@@ -53,54 +54,59 @@ int brightness[60] = {
   3,6,9,12,16,21,26,32,
   38,44,51,57};
 
-int displayColor[3];
+uint8_t displayColor[3];
 
 uint8_t neighbors[6];
 
-int team = 0;
+uint8_t team = 0; // which team are we part of blue or red (player 1 or player 2)
 
 uint32_t lastPressTime = 0;
 uint8_t bDidLongPress = 0;
 
-int gameStartTime = 0;
-int lifeSpan = 15000;        // 15 second lifespan
-int health = lifeSpan;       // starting life
-int moveBoost = 5000;        // 5 second boost when moved
+uint32_t gameStartTime = 0.0; // to know how far into the game we are
+float health = 100.0;         // 100% health for start of life
+float damageRate = 5.0;       // how much health is lost / second
+float moveBoost = 20.0;       // 20% health boost when moved
 
+uint32_t lastUpdateTime = 0;
+uint32_t updateFrequency = 50;  //milliseconds
 
 void setup() {
   // put your setup code here, to run once:
   setButtonCallback(button);
   setLongButtonCallback(longPress, 1000);
-  setState(4);  //waiting
-  setColor(colors[3]);  //waiting
+  setState(1);  // test starting as player 1
   setMicOff();
-  setTimeout(300);  // 2 minute
+  setTimeout(180);  // 3 minute
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  uint32_t curTime = getTimer();
 
   // check surroundings
   getNeighborStates(neighbors);
 
+  // if we are alive...
   if(getState() == 1 || getState() == 2) {
     // check how much life is remaining
-    health = lifeSpan - (getTimer() - gameStartTime);
-  
-    if(health <= 0) {
+    health -= damageRate * ((curTime - lastUpdateTime) / 1000.0);
+    // same as health = health - ....;
+ 
+    if(health <= 10.0) {
       // we are dead, show dead state
+      health = 0.0;
       setState(3);
     }
     else {
       // deal with a living tile here
 
-      int friends = 0;
-      int enemies = 0;
-      int numNeighbors = 0;
+      uint8_t friends = 0;
+      uint8_t enemies = 0;
+      uint8_t numNeighbors = 0;
       
       // count friends and enemies
-      for(int i=0; i<6; i++) {
+      for(uint8_t i=0; i<6; i++) {
         
         // count total number of neighbors
         if(neighbors[i] != 0) {
@@ -108,51 +114,88 @@ void loop() {
         }
         
         // check team red first
-        if(team == 0) {
-          if(neighbors[i] == 1) {
-            friends++;
+        if(team == 0) {                 // if on red team
+          if(neighbors[i] == 1) {       // if red neighbor
+            friends++;                  // count them as a friend
           }
-          else if(neighbors[i] == 2) {
-            enemies++;
+          else if(neighbors[i] == 2) {  // if blue neighbor
+            enemies++;                  // count them as an enemy
           }
         }
         // check team blue next
-        else if(team == 1) {
-          if(neighbors[i] == 2) {
-            friends++;
+        else if(team == 1) {            // if on blue team
+          if(neighbors[i] == 2) {       // if blue neighbor
+            friends++;                  // count them as a friend
           }
-          else if(neighbors[i] == 1) {
-            enemies++;
+          else if(neighbors[i] == 1) {  // if red neighbor
+            enemies++;                  // count them as an enemy
           }
         }
       }
 
       // determine rate of life based on surroundings (sad but true)
-      if(friends > enemies) {
+      if(numNeighbors == 0) {
+        // we are lonely or being moved. stay at the current damage rate
+      }
+      else if(friends > enemies) {
         // don't die quickly
+        damageRate = 5.0;
       }
       else if (friends == enemies) {
         // normal rate
+        damageRate = 5.0;
       }
       else { // implied friends < enemies
         // surrounded by enemies, this is gonna be rough
+        damageRate = 10.0;
       }
 
       
     }
   }
-  
+
+  // determine how fast we pulse (i.e. show health status)
+  float pulsePeriod = health * 30.0;
+  if(pulsePeriod <= 30.0) {
+    setState(3);  // die, die, die
+    pulsePeriod = 3000.0; // slow dead pulse period
+  }
+
+  uint16_t sinLoc = curTime % (int)pulsePeriod;  // integer between 0 and period
+  uint8_t idx = 60.0 * sinLoc / pulsePeriod;     // integer between 0 and 59
+
   // display your state based on the team you are on
-  if(team == 0) {
+  if(getState() == 3) {
+    // display state using white (dead white)
+    displayColor[0] = colors[2][0];
+    displayColor[1] = colors[2][1];
+    displayColor[2] = colors[2][2];
+  }
+  else if(team == 0) {
     // display state using red
-    setColor(colors[0]);
+    displayColor[0] = colors[0][0];
+    displayColor[1] = colors[0][1];
+    displayColor[2] = colors[0][2];
   } else if(team == 1){
     // display state using blue
-    setColor(colors[1]);
+    displayColor[0] = colors[1][0];
+    displayColor[1] = colors[1][1];
+    displayColor[2] = colors[1][2];
   } else {
     // how did we get here?
   }
+  
+  uint8_t r = 4 + (displayColor[0] * brightness[idx])/255.0;
+  uint8_t g = 4 + (displayColor[1] * brightness[idx])/255.0;
+  uint8_t b = 4 + (displayColor[2] * brightness[idx])/255.0;
+  
+  displayColor[0] = r; 
+  displayColor[1] = g; 
+  displayColor[2] = b;
+  
+  setColor(displayColor); 
 
+  lastUpdateTime = curTime;
 }
 
 
@@ -161,12 +204,21 @@ void loop() {
  * starts a game if waiting to play
  */
 void button() {
-  if(getState() == 4) {
-    // start the game
-    setState(5);
-    setColor(colors[3]);
+  // reset health
+  health = 100.0;
+  // set state based on team(color)
+  switch(team) {
+    case 0: setState(1); break;
+    case 1: setState(2); break;
+    default: setState(1); break;
   }
+
+//  if(getState() == 4) {
+//    // start the game
+//    setState(1);
+//  }
   lastPressTime = getTimer();
+  lastUpdateTime = getTimer();
   bDidLongPress = 0;
 }
 
