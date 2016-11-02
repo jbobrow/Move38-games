bed728a1f3be0442a88af982ac9f150faae32d72
Nick Bentley <nickbentley@Nicks-MacBook-Pro.local>
2016-11-02 16:43:09 -0500

    add long button press to switch initial team on flick tiles

--- FlickFlop/FlickFlop-Tiles/FlickFlop-Tiles.ino
+++ FlickFlop/FlickFlop-Tiles/FlickFlop-Tiles.ino
@@ -12,9 +12,14 @@ uint8_t neighbors[6];
 int numNeighbors = 0; 
 uint8_t colors[2][3] = {{204,0,0},  // Red - player 1             
                         {0,0,255}}; // Blue - player 2
+
+uint8_t team = 0;
+uint8_t numTeams = 2;
+                        
 void setup () {
   setColor(colors[1]);  //set initial color
   setState(0);          //set initial state
+  setLongButtonCallback(button, 1000); // change initial color
 }
 
 void loop () {  
@@ -35,9 +40,21 @@ void loop () {
     if (getState() == 1) {
       setColor(colors[0]); 
     }
-      if (getState() == 2) {
+    else if (getState() == 2) {
       setColor(colors[1]); 
     }
   }
 }
- 
+
+void button() {
+  // switch which team we are on
+  team = (team + 1) % numTeams;
+  
+  if (team == 0) {
+    setColor(colors[0]); 
+  }
+  else if (team == 1) {
+    setColor(colors[1]); 
+  }
+}
+
