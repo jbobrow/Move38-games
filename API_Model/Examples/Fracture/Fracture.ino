a060c0395a6671818bcd19e9543935a6df6ac900
Jonathan Bobrow <jbobrow@gmail.com>
2016-12-16 14:56:59 -0500

    udpate fracture to be closer to complete

--- API_Model/Examples/Fracture/Fracture.ino
+++ API_Model/Examples/Fracture/Fracture.ino
@@ -1,8 +1,17 @@
-// Simple Fracture
+/* Simple Fracture
+ *  
+ *  Rules:
+ *  Tiles are only happy when next to at least 2 tiles 
+ *  and none of their own color (state)
+ *  
+ */
+ 
+int numTeams = 6;
+Color colors[6] = {RED, BLUE, YELLOW, MAGENTA, CYAN, GREEN};
 
 void setup() {
   // put your setup code here, to run once:
-  
+  setState(1);
 }
 
 void loop() {
@@ -11,6 +20,7 @@ void loop() {
 
 void buttonTriplePressed() {
   // change team
+  setState((myState() + 1) % numTeams);
 }
 
 void neighborChanged() {
@@ -25,6 +35,6 @@ void neighborChanged() {
     blink(1000);
   }
   else {
-    setColor(colors[myState()];
+    setColor(colors[myState()]);
   }
 }
