24e8cf545f52c25826d127dd61bdaa57f026269a
jbobrow <jbobrow@gmail.com>
2017-01-12 14:27:41 -0500

    adding new game, flick form

---
+++ FlickForm/FlickForm.ino
@@ -0,0 +1,29 @@
+/*  Flick Form / Chimera (Game)
+ *  
+ *  Inspired by chemical bonds, this game rewards players for flicking their tiles into difficult to attach configurations
+ *  
+ *  2 center tiles are attached to share one side. They wait pulsing white together
+ *  (if these 2 tiles are alone, they are simply flashing white)
+ *  
+ *  Each player gets 2 tiles (team cyan and team magenta)
+ *  
+ *  Player 1 flicks each tile first attaching one, to make the mass 3 tiles
+ *  then flicks the second to make the mass 4 tiles
+ *  
+ *  When a tile attaches, all 3... or all 4 tiles flash n times, according to the score of the placement of the tile
+ *  
+ *  Player 2 does the same
+ *  
+ *  by Che-Wei + Jonathan
+ *  01.12.2017
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
